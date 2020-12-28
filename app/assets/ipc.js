6754c53fd4b0329abdc7eb32fd02c42bb5d365df
Adam El Messaoudi <aelmessaarab@gmail.com>
2020-12-28 08:54:34 +0100

    Improvements

--- app/assets/ipc.js
+++ app/assets/ipc.js
@@ -19,21 +19,3 @@ ipcRenderer.on('videostream', (event, arg) => {
     document.getElementById("imgstream").src = "data:image/jpg;base64," + arg;
     console.log(arg)
 });
-
-//La següent funció s'encarrega de que quan es clica intro en el input envia l'ordre a el backend
-var input = document.getElementById("input1");
-input.addEventListener('keyup', function (e) {
-    if (e.key === 'Enter' || e.keyCode === 13) {
-        ipcRenderer.send('command', input.value);
-        if (input.value == "stop_stream") {
-            document.getElementById("logs").style.display = "block";
-            document.getElementById("imgstream").style.display = "none";
-
-        }
-        input.value = "";
-    }
-});
-
-function commbutf(value) {
-    ipcRenderer.send('commbuts', value);
-}
